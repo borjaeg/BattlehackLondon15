2626786a5ba21e4797b49194c96625aea52d9254
Borja A. Espejo-Garcia <borja3790@gmail.com>
2015-04-26 02:06:00 +0200

    Procfile with heroku database updated with os imported version 2

--- db_create.py
+++ db_create.py
@@ -1,9 +1,21 @@
 import psycopg2
 import os
+import urlparse
 
-host = os.environ['DATABASE_URL']
+#host = os.environ['DATABASE_URL']
 
-conn = psycopg2.connect("dbname='challenge_for_people' user='root' host= " + host + " password='root'")
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
+#conn = psycopg2.connect("dbname='challenge_for_people' user='root' host= " + host + " password='root'")
 cursor = conn.cursor()
 query = """
 CREATE DATABASE challenges;

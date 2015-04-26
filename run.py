0bff3e4419923f00c95c0addbb7bc46008545673
Borja A. Espejo-Garcia <borja3790@gmail.com>
2015-04-26 09:28:30 +0200

    bug fixed

--- run.py
+++ run.py
@@ -25,14 +25,16 @@ braintree.Configuration.configure(
 )
 
 # Uncomment when deploy to heroku
-host = os.environ['DATABASE_URL']
-#host = 'localhost'
+#host = os.environ['DATABASE_URL']
+host = 'localhost'
 
 
 @app.route("/")
 def show_challenges():
 
 	try:
+		'''
+		
 		urlparse.uses_netloc.append("postgres")
 		url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
@@ -43,8 +45,8 @@ def show_challenges():
 		host=url.hostname,
 		port=url.port
 	)
-		
-		#conn = psycopg2.connect("dbname='challenge_for_people' user='root' host= " + host + " password='root'")
+	'''	
+		conn = psycopg2.connect("dbname='challenge_for_people' user='root' host= " + host + " password='root'")
 		cursor = conn.cursor()
 		query = "SELECT name FROM challenges;"
 		cursor.execute(query)
@@ -70,10 +72,16 @@ def register_event():
 	
 	return render_template("register_event.html")
 
+@app.route("/become_hero")
+def become_hero():
+	project = request.args.get('project', '')
+	
+	return render_template("register_event.html")
+
 
 @app.route("/challenge/<challenge>")
 def challenge(challenge):
-	
+	'''
 	urlparse.uses_netloc.append("postgres")
 	url = urlparse.urlparse(os.environ["DATABASE_URL"])
 	conn = psycopg2.connect(
@@ -84,9 +92,9 @@ def challenge(challenge):
 		port=url.port
 	)
 	
-	
+	'''
 	#project = request.args.get('project', '')
-	#conn = psycopg2.connect("dbname='challenge_for_people' user='root' host= " + host + " password='root'")
+	conn = psycopg2.connect("dbname='challenge_for_people' user='root' host= " + host + " password='root'")
 	cursor = conn.cursor()
 	token = client_token()
 	print token

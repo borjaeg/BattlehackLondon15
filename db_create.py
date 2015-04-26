b39cfc66287df1e1c7c5abecb65717f386423041
Borja A. Espejo-Garcia <borja3790@gmail.com>
2015-04-26 02:11:42 +0200

    Procfile with heroku database updated with os imported splitting queries

--- db_create.py
+++ db_create.py
@@ -17,11 +17,13 @@ conn = psycopg2.connect(
 
 #conn = psycopg2.connect("dbname='challenge_for_people' user='root' host= " + host + " password='root'")
 cursor = conn.cursor()
-query = """
-CREATE DATABASE challenges;
-USE challenges;
-DROP TABLE IF EXISTS challenges;
-CREATE TABLE challenges(
+query = "CREATE DATABASE challenges;"
+cursor.execute(query)
+query="USE challenges;"
+cursor.execute(query)
+query="DROP TABLE IF EXISTS challenges;"
+cursor.execute(query)
+query="""CREATE TABLE challenges(
   id_challenge SERIAL,
   name VARCHAR(100) NOT NULL,
   description TEXT,  -- NOT NULL,
@@ -32,8 +34,11 @@ CREATE TABLE challenges(
   proof VARCHAR(500),
   PRIMARY KEY(id_challenge)
 );
-INSERT INTO challenges(name, description, donators, levying, proof) VALUES ('Run For Ebro', 'Ebro river has had problems in the last month because of the huge amount of rain. I want to help my Zaragoza friends running 30 km. Do you want to run with me? Donate!', 52, 5000, 'https://www.youtube.com/embed/HgzGwKwLmgM');
-INSERT INTO challenges(name, description, donators, levying, proof) VALUES ('Hack For Ebola', 'Ebola is an important disease. Hack with me', 51042, 1500000, 'https://www.youtube.com/embed/zO6D_BAuYCI');
 """
 cursor.execute(query)
+query="INSERT INTO challenges(name, description, donators, levying, proof) VALUES ('Run For Ebro', 'Ebro river has had problems in the last month because of the huge amount of rain. I want to help my Zaragoza friends running 30 km. Do you want to run with me? Donate!', 52, 5000, 'https://www.youtube.com/embed/HgzGwKwLmgM');"
+cursor.execute(query)
+query="INSERT INTO challenges(name, description, donators, levying, proof) VALUES ('Hack For Ebola', 'Ebola is an important disease. Hack with me', 51042, 1500000, 'https://www.youtube.com/embed/zO6D_BAuYCI');"
+cursor.execute(query)
+
 conn.close()

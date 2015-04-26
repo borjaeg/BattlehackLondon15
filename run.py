a7a5682c9765c9c9effc1de4e4670109d8df0015
Borja A. Espejo-Garcia <borja3790@gmail.com>
2015-04-26 12:03:31 +0200

    Data modified and database integration with donations

--- run.py
+++ run.py
@@ -25,15 +25,15 @@ braintree.Configuration.configure(
 )
 
 # Uncomment when deploy to heroku
-host = os.environ['DATABASE_URL']
-#host = 'localhost'
+#host = os.environ['DATABASE_URL']
+host = 'localhost'
+#global_challenge = ''
 
 
 @app.route("/")
 def show_challenges():
 
 	try:
-		
 		urlparse.uses_netloc.append("postgres")
 		url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
@@ -53,7 +53,6 @@ def show_challenges():
 		return render_template("challenges.html", challenges = challenges)
 	except:
 		logging.exception("Something awful happened!")
-		print "I am unable to connect to the database"
 		return "-1"
 		
 
@@ -80,6 +79,7 @@ def become_hero():
 
 @app.route("/challenge/<challenge>")
 def challenge(challenge):
+	
 	urlparse.uses_netloc.append("postgres")
 	url = urlparse.urlparse(os.environ["DATABASE_URL"])
 	conn = psycopg2.connect(
@@ -94,6 +94,8 @@ def challenge(challenge):
 	cursor = conn.cursor()
 	token = client_token()
 	print token
+	session['challenge'] = challenge
+	print "global_challenge %s" % session['challenge']
 	query = "SELECT * FROM challenges WHERE name = '%s'" % challenge
 	print challenge
 	cursor.execute(query)
@@ -109,10 +111,33 @@ def client_token():
 def create_checkout():
 	print "create checkout"
 	nonce = request.form["payment_method_nonce"]
+	amount = request.form["amount"]
+	amount = amount[1:]
+	print amount
+
 	result = braintree.Transaction.sale({
-		"amount": "1.00",
+		"amount": str(amount),
 		"payment_method_nonce": nonce
 	})
+
+	urlparse.uses_netloc.append("postgres")
+	url = urlparse.urlparse(os.environ["DATABASE_URL"])
+	conn = psycopg2.connect(
+		database=url.path[1:],
+		user=url.username,
+		password=url.password,
+		host=url.hostname,
+		port=url.port
+	)
+	
+	#conn = psycopg2.connect("dbname='challenge_for_people' user='root' host= " + host + " password='root'")
+	print "global challenge %s" % session['challenge']
+	query = "UPDATE challenges SET levying = levying + %d, donators = donators + 1 WHERE name = '%s'" % (int(amount), session['challenge'])
+	print challenge
+	cursor = conn.cursor()
+	cursor.execute(query)
+	conn.commit()
+	conn.close()
 	return render_template("thanks.html");
 
 if __name__ == "__main__":
